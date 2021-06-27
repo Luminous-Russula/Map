dec326ed0e455115ecbd07b4e04a9ef311026ed9
PapersNotIncluded <spike.m@mail.ru>
2021-06-28 01:36:21 +0300

    Beta version of heart button

--- scroll.js
+++
@@ -1,9 +0,0 @@
-let scroll = document.querySelector('.scrollbar')
-
-let scrollH = scroll.getBoundingClientRect().height
-let H = document.body.scrollHeight - window.innerHeight
-
-window.onscroll = (e)=>{
-    let h = (window.pageYOffset) / (H + scrollH*2)
-    scroll.style.top = `${h*100}%`
-}
