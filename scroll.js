ddf23a627e91f0cedb1c36b0ec693a5fb4d5aaf8
Blank <holoparrentattachment@gmail.com>
2021-06-27 20:48:46 +0300

    Smooth scroll on go button

--- scroll.js
+++ scroll.js
@@ -8,3 +8,30 @@ window.onscroll = (e)=>{
     scroll.style.top = `${h*100}%`
 }
 
+let anchor = document.querySelector('.level')
+let gobtn = document.querySelector('.main__gobtn')
+let cury, targy
+
+gobtn.onclick = (e)=>{
+    cury = window.pageYOffset
+    targy = anchor.getBoundingClientRect().top - 10
+    
+    gobtn.smoothScroll = setInterval(()=>{
+        cury = cury + (targy - cury)/25 + 5
+        window.scrollTo(0, cury)
+        
+        if(Math.abs(cury - targy) < 10){
+            window.scrollTo(0, targy)
+            clearInterval(gobtn.smoothScroll)
+        }
+    }, 10)
+}
+
+
+
+
+
+
+
+
+
